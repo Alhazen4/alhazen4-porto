4996f7c1f0ca36d265fb548ee5e0ca9c45d7458f
Alhazen4 <cybernonk404@gmail.com>
2023-10-18 13:28:45 +0700

    Add docs folder for production

---
+++ docs/assets/data.ts
@@ -0,0 +1,37 @@
+export const projects = [
+    { 
+      id: 1, 
+      projectName: 'Packet Web App',
+      projectPosition: 'Fullstack Developer',
+      projectDuration: '3 Months',
+      projectDescription: 'Create an application for data collection on packages that come to the office by designing both the frontend and database',
+      projectLogo: 'logo/levelup_logo.png',
+      projectBgColor: 'linear-gradient(180deg, rgba(54, 96, 174, 1) 0%, rgba(193, 76, 152, 1) 100%)',
+      projectTechLogo: ['vue.png', 'nodejs.png', 'expressjs.png', 'mysql.png'],
+      projectResult: ['paket-app/paket-1.png', 'paket-app/paket-2.png', 'paket-app/paket-3.png']
+    },
+
+    { 
+      id: 2, 
+      projectName: 'BCA IT Intern',
+      projectPosition: 'Frontend Developer',
+      projectDuration: '3 Months',
+      projectDescription: 'Help to develop the frontend component library used by the application development inside the company',
+      projectLogo: 'logo/itbca_logo.png',
+      projectBgColor: '#2d2c2f',
+      projectTechLogo: ['angular.png', 'typescript.png', 'gitlab.png'],
+      projectResult: ['lib-comp/lib-comp1.png', 'lib-comp/lib-comp2.png', 'lib-comp/lib-comp3.png']
+    },
+  
+    { 
+      id: 3, 
+      projectName: 'Programmer Intern',
+      projectPosition: 'Laravel Developer',
+      projectDuration: '6 Months',
+      projectDescription: 'Debugging and develop the outsourcing website owned by the company',
+      projectLogo: 'logo/upscale_logo.png',
+      projectBgColor: '#31517e',
+      projectTechLogo: ['php.png', 'laravel.png', 'mysql.png', 'gitlab.png'],
+      projectResult: ['upscale-clone/upscale-1.png', 'upscale-clone/upscale-2.png', 'upscale-clone/upscale-3.png']
+    },
+  ];
